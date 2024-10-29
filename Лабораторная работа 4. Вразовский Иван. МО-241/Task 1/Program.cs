283c3251ace09b101280a0733d10959a99f1065e
Vrazzx <ivan.vraz@gmail.com>
2024-10-29 22:29:59 +0600

    Add files via upload

---
+++ Лабораторная работа 4. Вразовский Иван. МО-241/Task 1/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+class Program{
+    static void Main(){
+        Console.WriteLine("Введите вашу последовательность");
+        int n = 1;
+        int n1 = 0;
+        int new_num = 0;
+        int count = 0;
+
+        while(n>0){
+            n = Convert.ToInt32(Console.ReadLine());
+            if(n%10 == 0){
+                n = n/10;
+            }
+            if(n<=0){
+                break;
+            }
+            new_num = n;
+            while(new_num>0){
+                n1 = new_num%10;
+                if(n1%2 == 0){
+                    count = count + 1;
+                    Console.Write(n1);
+                }
+                new_num = new_num/10;
+            }
+            if(count == 0){
+                    Console.WriteLine("Чётных нет");
+                
+            }
+            Console.WriteLine("");
+            count = 0;
+
+        }
+    }
+}
