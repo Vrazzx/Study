c6ea90d6b256d16df99ad8828210a91576cc8855
Vrazzx <ivan.vraz@gmail.com>
2024-12-16 18:04:39 +0600

    Add files via upload

---
+++ Лабораторная работа 6. Вразовский Иван. МО-241/Program.cs
@@ -0,0 +1,83 @@
+﻿
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Введите количество массивов");
+        int n = int.Parse(Console.ReadLine());
+        // int count = 0;
+        int max = int.MinValue;
+        int min = int.MaxValue;
+        bool checkMax = false;
+        bool checkMin = false;
+        int[][] stepArray = new int[n][];
+        for (int i = 0; i < n; i++)
+        {
+            Console.WriteLine("Введите элементы подмассива через пробел");
+            string input = Console.ReadLine();
+            string[] stringNumbers = input.Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries);
+            stepArray[i] = new int[stringNumbers.Length];
+            for (int j = 0; j < stringNumbers.Length; j++)
+            {
+                stepArray[i][j] = int.Parse(stringNumbers[j]);
+            }
+        }
+        for (int i = 0; i < n; i++)
+        {
+            for (int j = 0; j < stepArray[i].Length; j++)
+            {
+
+                if (j == 0 && i != 0)
+                {
+                    if (checkMax == true && checkMin == true)
+                    {
+                        Console.WriteLine($"Строка, содержащая четный максимальный и минимальный элемент имеет номер {i}. Максимум {max} Минимум {min}");
+                    }
+                    else{
+                        Console.WriteLine($"Строка {i} не содержит четные и максимум и минимум. Максимум {max} Минимум {min}");
+                    }
+                    max = int.MinValue;
+                    min = int.MaxValue;
+                }
+                if (stepArray[i][j] > max)
+                {
+                    max = stepArray[i][j];
+                    if (stepArray[i][j] % 2 == 0)
+                    {
+                        checkMax = true;
+                    }
+                    else
+                    {
+                        checkMax = false;
+                    }
+
+                }
+                if (stepArray[i][j] < min)
+                {
+                    min = stepArray[i][j];
+                    if (stepArray[i][j] % 2 == 0)
+                    {
+                        checkMin = true;
+
+                    }
+                    else
+                    {
+                        checkMin = false;
+                    }
+
+                }
+
+            }
+        }
+        if (checkMax == true && checkMin == true)
+        {
+            Console.WriteLine($"Строка, содержащая четный максимальный и минимальный элемент имеет номер {n}. Максимум {max} Минимум {min}");
+        }
+        else{
+            Console.WriteLine($"Строка {n} не содержит четные и максимум и минимум. Максимум {max} Минимум {min}");
+        }
+
+
+    }
+}
---
+++ Лабораторная работа 7. Вразовский Иван. МО-241/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+class HelloWorld {
+  static void Main() {
+      Console.WriteLine("Введите строку с множеством пробелов");
+      string input = Console.ReadLine();
+      string formatted = FormattedString(input);
+      int count = 0;
+      int countTryWords = 0;
+      int vowelCount = 0;
+      int consonantCount = 0;
+      Console.WriteLine(formatted);
+      string[] words = Words(input);
+      string vowels = "aeyuioAEYUIO";
+      
+      foreach(string word in words){
+          foreach(char c in word){
+              if(char.IsLetter(c)){
+                  if(vowels.Contains(c)){
+                      vowelCount++;
+                  }
+                  else{
+                      consonantCount++;
+                  }
+              }
+              
+          }
+          if(vowelCount > consonantCount){
+                  countTryWords++;
+              }
+          vowelCount = 0;
+          consonantCount = 0;
+          count++;
+          if(IsPalindrome(word)){
+              Console.WriteLine($"Слово {word} является полиндромом");
+          }
+          else{
+              Console.WriteLine($"Слово {word} не является полиндромом");
+          }
+      }
+      Console.WriteLine($"Количество слов {count}");
+      Console.WriteLine($"Количество слов, в которых количество согласных букв меньше чем гасных {countTryWords}");
+  }
+  static string FormattedString(string input){
+      string[] words = input.Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries);
+      return string.Join(" ", words);
+  }
+  public static string[] Words(string input){
+      string[] words = input.Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries);
+      return words;
+  }
+  static bool IsPalindrome(string word){
+      if(string.IsNullOrEmpty(word)){
+          return false;
+      }
+      string cleanWord = word.Replace(" ", "").ToLower();
+      int length = cleanWord.Length;
+      for(int i = 0; i < length / 2; i++){
+          if (cleanWord[i] != cleanWord[length - 1 - i]){
+              return false;
+          }
+      }
+      return true;
+  }
+}
