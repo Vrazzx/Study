e07dc3eaeec7e82774429363e85e1b577d3cf697
Vrazzx <ivan.vraz@gmail.com>
2024-12-23 22:13:44 +0600

    Add files via upload

---
+++ Лабораторная работа 10. Вразовский Иван. МО-241/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+public class MyClass 
+{
+    private int firstField;
+    private int secondField;
+    
+    public MyClass(){
+        firstField = 0;
+        secondField = 0;
+    }
+    public MyClass(int first){
+        firstField = first;
+        secondField = 0;
+    }
+    public MyClass(int first, int second){
+        firstField = first;
+        secondField = second;
+    }
+    
+    public int Sum(){
+        return firstField + secondField;
+    }
+    public int Muplity(){
+        return firstField * secondField;
+    }
+    public string Divide(){
+        if (secondField == 0){
+            return "Error";
+        }
+        return(firstField / secondField).ToString();
+    }
+    public int Subtract(){
+        return firstField - secondField;
+    }
+    
+}
+ 
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Введите две переменные");
+        int a = int.Parse(Console.ReadLine());
+        int b = int.Parse(Console.ReadLine());
+        MyClass obj1 = new MyClass();
+        int a = int.Parse(Console.ReadLine());
+        MyClass obj2 = new MyClass(a);
+        int c = int.Parse(Console.ReadLine());
+        int b = int.Parse(Console.ReadLine());
+        MyClass obj3 = new MyClass(c, b);
+        
+        MyClass[] oblects = { obj1, obj2, obj3 };
+        
+        foreach (var obj in oblects)
+        {
+            Console.WriteLine($"Сумма: {obj.Sum()}");
+            Console.WriteLine($"Произведение: {obj.Muplity()}");
+            Console.WriteLine($"Деление: {obj.Divide()}");
+            Console.WriteLine($"Разность: {obj.Subtract()}");
+            Console.WriteLine();
+        }
+    }
+}
---
+++ Лабораторная работа 11. Вразовский Иван. МО-241/Program.cs
@@ -0,0 +1,117 @@
+﻿using System;
+class Planet
+{
+    public string Name { get; }
+    public double DistanceFromCenter { get; }
+    public double Diameter { get; }
+    public int NumbersOfMoon { get; }
+
+    public Planet(string name, double distanceFromCenter, double diameter, int numbersOfMoon)
+    {
+        Name = name;
+        DistanceFromCenter = distanceFromCenter;
+        Diameter = diameter;
+        NumbersOfMoon = numbersOfMoon;
+    }
+
+    public void DisplayInfo()
+    {
+        Console.WriteLine($"Название: {Name}, Расстояние: {DistanceFromCenter}, Диаметер: {Diameter}, Спутники: {NumbersOfMoon}");
+    }
+
+}
+
+class Program
+{
+    private static Planet[] planets = new Planet[100];
+    private static int count = 0;
+    static void Main(string[] args)
+    {
+       int choice;
+       do
+       {
+        System.Console.WriteLine("Меню: ");
+        System.Console.WriteLine("1. Заполнение базы данных");
+        System.Console.WriteLine("2. Первая выборка");
+        System.Console.WriteLine("3. Вторая выборка");
+        System.Console.WriteLine("4. Выход");
+        System.Console.Write("Выберете пункт меню: ");
+        choice = int.Parse(System.Console.ReadLine());
+
+        switch (choice)
+        {
+            case 1:
+                FillDatabase();
+                break;
+            case 2:
+                SelectByMoons();
+                break;
+            case 3:
+                SelectByDistance();
+                break;
+            case 4:
+                System.Console.WriteLine("Выход из программы...");
+                break;
+            default:
+                System.Console.WriteLine("Неверный выбор, попробуйтен ещё раз. ");
+                break;
+        }
+        
+       } while(choice != 4);
+    
+    }
+
+    static void FillDatabase()
+    {
+        System.Console.Write("Введите количество планет для добавления их в базу даных");
+        int num = int.Parse(System.Console.ReadLine());
+        for (int i = 0; i < num; i++)
+        {
+            System.Console.Write("Введите название планеты: ");
+            string name = Console.ReadLine();
+            System.Console.Write("Введите удаленность планеты от центра галактики: " );
+            double distance = double.Parse(Console.ReadLine());
+            System.Console.Write("Введите диаметр планеты: ");
+            double diameter = double.Parse(Console.ReadLine());
+            System.Console.Write("Введите количество спутников: ");
+            int moons = int.Parse(Console.ReadLine());
+
+            planets[count++] = new Planet(name, distance, diameter, moons);
+        }
+    }
+    static void SelectByMoons()
+    {
+        if (count == 0)
+        {
+            System.Console.WriteLine("База данных пуста. Для начала, заполните её");
+            return;
+        }
+        System.Console.Write("Введите минимальное количество спутников: ");
+        int minMoons = int.Parse(System.Console.ReadLine());
+        for (int i = 0; i < count; i++)
+        {
+            if(planets[i].NumbersOfMoon >= minMoons)
+            {
+                planets[i].DisplayInfo();
+            }
+        }
+    }
+    
+    static void SelectByDistance()
+    {
+        if (count == 0)
+        {
+            System.Console.WriteLine("База данных пуста. Для начала, заполните её");
+            return;
+        }
+        System.Console.Write("Введите минимальную удалённость: ");
+        double minDistance = double.Parse(Console.ReadLine());
+        for(int i = 0; i < count; i++)
+        {
+            if(planets[i].DistanceFromCenter > minDistance)
+            {
+                planets[i].DisplayInfo();
+            }
+        }
+    }
+}
